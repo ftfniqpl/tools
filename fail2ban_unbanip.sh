dfc410f5ed0341ebc20fa01af983d4a3c1f149a7
tony <birdaccp@gmail.com>
2018-11-23 10:34:32 +0800

    add fail2ban unbanip script

---
+++ fail2ban_unbanip.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+clear
+#CheckIfRoot
+[ $(id -u) != "0" ] && { echo "${CFAILURE}Error: You must be root to run this script${CEND}"; exit 1; }
+
+fail2ban-client set ssh-iptables unbanip $1
+fail2ban-client set sshd unbanip $1
