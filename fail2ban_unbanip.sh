d21fe62b9b142078aaac3b07daa153ffc6ad1542
tony <birdaccp@gmail.com>
2018-11-23 10:40:09 +0800

    .

--- fail2ban_unbanip.sh
+++ fail2ban_unbanip.sh
@@ -4,5 +4,7 @@ clear
 #CheckIfRoot
 [ $(id -u) != "0" ] && { echo "${CFAILURE}Error: You must be root to run this script${CEND}"; exit 1; }
 
+fail2ban-client status ssh-iptables
+
 fail2ban-client set ssh-iptables unbanip $1
 fail2ban-client set sshd unbanip $1
